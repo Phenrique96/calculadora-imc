32dec1bd794a0bf16dbd97d049c47da247e96867
Paulo Henrique <99408681+Phenrique96@users.noreply.github.com>
2023-03-07 20:00:48 -0300

    Criando as primeiras funções em JS

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+function calculate() {
 
+    let nome = document.getElementById('name').value;
+    let idade = document.getElementById('age').value;
+    let peso = document.getElementById('quilo').value;
+    let altura = document.getElementById('metro').value;
+
+    if(nome == "" | idade == 0) { 
+        alert("Preencha os campos!")
+        return;
+    }
+
+    let total = peso / (altura * altura);
+
+    alert(nome + " está com " + idade + " anos e com " + total.toFixed(0) + " de IMC.");
+
+}
+
+let botao = document.getElementById('submit');
+
+botao.addEventListener("click", calculate)
